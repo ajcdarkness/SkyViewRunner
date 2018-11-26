f2c2225c3f623d5c52e98c63fa5d5baaa5356a95
BT_1N3_16@bktech.local <BT_1N3_16@bktech.local>
2018-11-26 11:46:19 -0500

    Initial commit

---
+++ src/com/company/SkyView.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class SkyView {
+    private double[][] view;
+
+    public SkyView(int numRows, int numCols, double[] scanned){
+        view = new double[numRows][numCols];
+        int pos = 0;
+
+        for(int i = 0; i < view.length; i++){
+            if(i % 2 == 0){
+                for(int j = 0; j < view[i].length; j++){
+                    view[i][j] = scanned[pos];
+                    pos++;
+                }
+            }else{
+                for(int j = view[i].length-1; j>= 0; j--){
+                    view[i][j] = scanned[pos];
+                    pos++;
+                }
+            }
+        }
+    }
+    public double getAverage(int startRow, int endRow, int startCol, int endCol){
+        double total = 0.0;
+        int count = 0;
+        for(int i = startRow; i <= endRow; i++){
+            for(int j = startCol; j <= endCol; j++){
+                total += this.view[i][j];
+                count++;
+            }
+        }
+        return (double)(total/count);
+    }
+}
